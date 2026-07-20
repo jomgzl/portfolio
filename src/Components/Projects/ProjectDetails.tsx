c829a9fb88fe854c81bbdce374444d4a9aeb32fc
Jomgzl <joseph.mogaizel@gmail.com>
2026-07-20 17:48:23 +0200

    Fixed background color in Project details. Removed an unused component. Cleaned some code

--- src/Components/Projects/ProjectDetails.tsx
+++ src/Components/Projects/ProjectDetails.tsx
@@ -1,56 +1,11 @@
-// import { useParams } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import styles from "./ProjectDetails.module.css";
 import { useTranslation, Trans } from "react-i18next";
 
 function ProjectDetails() {
-	// const { projectId } = useParams();
 	const location = useLocation();
 	const { id, logo, github } = { ...location.state };
 	const { t } = useTranslation();
-
-	// const keywords = [
-	// 	"code",
-	// 	"task",
-	// 	"app",
-	// 	"simple",
-	// 	"straightforward",
-	// 	"efficient.",
-	// 	"Node.js,",
-	// 	"Express.js",
-	// 	"MySQL2",
-	// 	"built",
-	// 	"scratch.",
-	// 	"build",
-	// 	"website",
-	// 	"scratch,",
-	// 	"represent",
-	// 	"showcase",
-	// 	"skills",
-	// 	"profile.",
-	// 	"React,",
-	// 	"Vite",
-	// 	"TypeScript.",
-	// 	"quest",
-	// 	"passion",
-	// 	"programming,",
-	// 	"password",
-	// 	"generator",
-	// 	"Python",
-	// 	"multiple",
-	// 	"generation",
-	// 	"options.",
-	// ];
-
-	// const customizedDescription = t(`projects.${id}.description`)
-	// 	.split(" ")
-	// 	.map((word: string) => {
-	// 		return (
-	// 			<span>
-	// 				{keywords.includes(word) ? <strong>{word}</strong> : word}{" "}
-	// 			</span>
-	// 		);
-	// 	});
 
 	return (
 		<div className={styles.containerGeneralProjectDetails}>
